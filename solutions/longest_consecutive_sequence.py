507aec9f797e138212f0c09cdd59f5ec596afb9c
Charlie Ward <charlie33ward@gmail.com>
2024-11-20 12:36:32 -0800

    Three int sum

--- solutions/longest_consecutive_sequence.py
+++ solutions/longest_consecutive_sequence.py
@@ -1,5 +1,22 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        
+        numSet = set(nums)
+        longest = 0
+
+        for n in nums:
+            if (n - 1) not in numSet:
+                length = 0
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+
+        return longest
+        
+        
+        """
+        ORIGINAL SOLUTION: invalid, runs in o(nlogn)
+
         if not nums:
             return 0
         
@@ -17,4 +34,4 @@ class Solution:
             res = max(sequenceCounter, res)
             prev = sortedList[i]
         
-        return res
+        return res"""
